5a318541267a8e7b6ab9e43b261db2ca7f7e6490
ethanshedd <eshedd20@dtechhs.org>
2019-03-25 14:32:55 -0700

    initial

---
+++ Drawer/Assets/Scripts/Visibility.cs
@@ -0,0 +1,33 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+ 
+ public class ImageShow : MonoBehaviour {
+ 
+     public bool isImgOn;
+     public Image img;
+ 
+     void Start () {
+ 
+         img.enabled = true;
+         isImgOn = true;
+     }
+ 
+     void Update () {
+     
+         if (Input.GetKeyDown ("i")) {
+ 
+             if (isImgOn == true) {
+ 
+                 img.enabled = false;
+                 isImgOn = false;
+             }
+ 
+             else {
+ 
+                 img.enabled = true;
+                 isImgOn = true;
+             }
+         }
+     }
+ }
