6d9ff685a247b0141dce9aff3519c97a30852775
ethanshedd <eshedd20@dtechhs.org>
2019-03-27 15:45:13 -0700

    Added content and text

--- Drawer/Assets/Scripts/Cabinet.cs
+++ Drawer/Assets/Scripts/Cabinet.cs
@@ -21,6 +21,10 @@ public class Cabinet : MonoBehaviour {
     public Sprite ethan2;
     public Sprite ethan3;
     public Sprite ethan4;
+    public Sprite aiden1;
+    public Sprite aiden2;
+    public Sprite aiden3;
+    public Sprite aiden4;
     public Sprite dominic1;
     public Sprite dominic2;
     public Sprite dominic3;
@@ -29,10 +33,14 @@ public class Cabinet : MonoBehaviour {
     public Sprite daniel2;
     public Sprite daniel3;
     public Sprite daniel4;
-    public Sprite aiden1;
-    public Sprite aiden2;
-    public Sprite aiden3;
-    public Sprite aiden4;
+    public Sprite sam1;
+    public Sprite sam2;
+    public Sprite sam3;
+    public Sprite sam4;
+    public string url1;
+    public string url2;
+    public string url3;
+    public string url4;
 
     public void OnClick1 () {
         if (open1.activeSelf) {
@@ -89,66 +97,99 @@ public class Cabinet : MonoBehaviour {
     }
 
     public void OnClick5 () {
-        if (open4.activeSelf) {
-
-        } else {
-
-        }
+        Application.OpenURL (url1);
     }
     public void OnClick6 () {
-        if (open4.activeSelf) {
-
-        } else {
-
-        }
+        Application.OpenURL (url2);
     }
     public void OnClick7 () {
-        if (open4.activeSelf) {
-
-        } else {
-
-        }
+        Application.OpenURL (url3);
     }
     public void OnClick8 () {
-        if (open4.activeSelf) {
-
-        } else {
-
-        }
+        Application.OpenURL (url4);
     }
     // Start is called before the first frame update
     void Start () {
-        int person = URLParameters.GetSearchParameters().GetInt ("person", 2);
+        int person = URLParameters.GetSearchParameters ().GetInt ("person", 2);
         switch (person) {
             case 1:
                 {
-                    project1.GetComponent<SpriteRenderer>().sprite = ethan1;
-                    project2.GetComponent<SpriteRenderer>().sprite = ethan2;
-                    project3.GetComponent<SpriteRenderer>().sprite = ethan3;
-                    project4.GetComponent<SpriteRenderer>().sprite = ethan4;
+                    project1.GetComponent<Image> ().sprite = ethan1;
+                    project2.GetComponent<Image> ().sprite = ethan2;
+                    project3.GetComponent<Image> ().sprite = ethan3;
+                    project4.GetComponent<Image> ().sprite = ethan4;
+                    project1.transform.GetChild (0).GetComponent<Text> ().text = "HelpOut";
+                    project2.transform.GetChild (0).GetComponent<Text> ().text = "Personal Site";
+                    project3.transform.GetChild (0).GetComponent<Text> ().text = "Robotics Scouting App";
+                    project4.transform.GetChild (0).GetComponent<Text> ().text = "Currency Trader";
+                    url1 = "https://eshedd.github.io/HelpOut/";
+                    url2 = "https://eshedd.github.io/WebDesignClass/";
+                    url3 = "https://github.com/eshedd/ScoutingAppRobotics";
+                    url4 = "https://coinshedd.weebly.com/";
                 }
                 break;
             case 2:
                 {
-                    project1.GetComponent<SpriteRenderer>().sprite = aiden1;
-                    project2.GetComponent<SpriteRenderer>().sprite = aiden2;
-                    project3.GetComponent<SpriteRenderer>().sprite = aiden3;
-                    project4.GetComponent<SpriteRenderer>().sprite = aiden4;
+                    project1.GetComponent<Image> ().sprite = aiden1;
+                    project2.GetComponent<Image> ().sprite = aiden2;
+                    project3.GetComponent<Image> ().sprite = aiden3;
+                    project4.GetComponent<Image> ().sprite = aiden4;
+                    project1.transform.GetChild (0).GetComponent<Text> ().text = "Personal Site";
+                    project2.transform.GetChild (0).GetComponent<Text> ().text = "TEDx App";
+                    project3.transform.GetChild (0).GetComponent<Text> ().text = "TEDx DTHS";
+                    project4.transform.GetChild (0).GetComponent<Text> ().text = "Personal Cloud Platform";
+                    url1 = "http://aidenappleby.com";
+                    url2 = "https://app.tedxdesigntechhighschool.us";
+                    url3 = "http://edxdesigntechhighschool.us";
+                    url4 = "http://aiden.cloud";
                 }
                 break;
             case 3:
                 {
-
+                    project1.GetComponent<Image> ().sprite = dominic1;
+                    project2.GetComponent<Image> ().sprite = dominic2;
+                    project3.GetComponent<Image> ().sprite = dominic3;
+                    project4.GetComponent<Image> ().sprite = dominic4;
+                    project1.transform.GetChild (0).GetComponent<Text> ().text = "Random Game";
+                    project2.transform.GetChild (0).GetComponent<Text> ().text = "Softbody Pong";
+                    project3.transform.GetChild (0).GetComponent<Text> ().text = "Pong and Breakout Site";
+                    project4.transform.GetChild (0).GetComponent<Text> ().text = "Physics Engine";
+                    url1 = "https://github.com/Dominic-b/Pong/tree/secondary ";
+                    url2 = "https://github.com/Dominic-b/Project-Game ";
+                    url3 = "https://github.com/Dominic-b/Pong-Website";
+                    url4 = "https://github.com/Dominic-b/Physics-Engine";
                 }
                 break;
             case 4:
                 {
-
+                    project1.GetComponent<Image> ().sprite = daniel1;
+                    project2.GetComponent<Image> ().sprite = daniel2;
+                    project3.GetComponent<Image> ().sprite = daniel3;
+                    project4.GetComponent<Image> ().sprite = daniel4;
+                    project1.transform.GetChild (0).GetComponent<Text> ().text = "HelpOut";
+                    project2.transform.GetChild (0).GetComponent<Text> ().text = "Personal Site";
+                    project3.transform.GetChild (0).GetComponent<Text> ().text = "Robotics Scouting App";
+                    project4.transform.GetChild (0).GetComponent<Text> ().text = "Currency Trader";
+                    url1 = "";
+                    url2 = "";
+                    url3 = "";
+                    url4 = "";
                 }
                 break;
             case 5:
                 {
-
+                    project1.GetComponent<Image> ().sprite = sam1;
+                    project2.GetComponent<Image> ().sprite = sam2;
+                    project3.GetComponent<Image> ().sprite = sam3;
+                    project4.GetComponent<Image> ().sprite = sam4;
+                    project1.transform.GetChild (0).GetComponent<Text> ().text = "HelpOut";
+                    project2.transform.GetChild (0).GetComponent<Text> ().text = "Personal Site";
+                    project3.transform.GetChild (0).GetComponent<Text> ().text = "Robotics Scouting App";
+                    project4.transform.GetChild (0).GetComponent<Text> ().text = "Currency Trader";
+                    url1 = "";
+                    url2 = "";
+                    url3 = "";
+                    url4 = "";
                 }
                 break;
         }
